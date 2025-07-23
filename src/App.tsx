5e23de6b67328af16246077753ea40abce411686
neXet Lab <133590093+TanzinAbdul@users.noreply.github.com>
2025-07-23 22:44:48 +0600

    updated hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
